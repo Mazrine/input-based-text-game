b9bdd74ad434084b7044d1ef242468ca6c375bab
Lisa Mascarenhas Gusmão <lyhr.musi@gmail.com>
2023-10-24 20:45:31 -0300

    level up art!
    - add background gif made by me
    - changed colors
    - add spinning ring
    - minor fixes

--- script.js
+++ script.js
@@ -22,3 +22,35 @@ form.addEventListener("submit", (event) => {
     input.value = ""
     event.preventDefault();
 })
+
+const degreeToRadian = (angle) => {
+    return angle * (Math.PI / 180);
+  };
+  
+  const radius = 30;
+  const diameter = radius * 2;
+  
+  const circle = document.querySelector("#circle");
+  circle.style.width = `${diameter}px`;
+  circle.style.height = `${diameter}px`;
+  
+  const text = circle.dataset.text;
+  const characters = text.split("");
+  
+  const deltaAngle = 390 / characters.length;
+  const characterOffsetAngle = 8;
+  let currentAngle = -360;
+  
+  characters.forEach((character, index) => {
+    const span = document.createElement("span");
+    span.innerText = character;
+    const xPos = radius * (1 + Math.cos(degreeToRadian(currentAngle)));
+    const yPos = radius * (1 + Math.sin(degreeToRadian(currentAngle)));
+  
+    const transform = `translate(${xPos}px, ${yPos}px)`;
+    const rotate = `rotate(${(index * deltaAngle) + characterOffsetAngle}deg)`;
+    span.style.transform = `${transform} ${rotate}`;
+  
+    currentAngle += deltaAngle;
+    circle.appendChild(span);
+  });
