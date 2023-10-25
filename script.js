a22a1dbe1c303ed9bbbca3a961f4b5db62d50e52
Lisa Mascarenhas Gusmão <lyhr.musi@gmail.com>
2023-10-25 20:32:52 -0300

    Themes and carrosels
    - add carrosel system
    - add 4 themes
    - some css and html changes

--- script.js
+++ script.js
@@ -23,6 +23,84 @@ form.addEventListener("submit", (event) => {
     event.preventDefault();
 })
 
+const slider = document.querySelector(".items");
+		const slides = document.querySelectorAll(".item");
+		const button = document.querySelectorAll(".button");
+
+		let current = 0;
+		let prev = 4;
+		let next = 1;
+
+		for (let i = 0; i < button.length; i++) {
+			button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+		}
+
+		const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+
+		const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
+
+		const gotoNum = number => {
+			current = number;
+			prev = current - 1;
+			next = current + 1;
+
+			for (let i = 0; i < slides.length; i++) {
+				slides[i].classList.remove("active");
+				slides[i].classList.remove("prev");
+				slides[i].classList.remove("next");
+			}
+
+			if (next == 5) {
+				next = 0;
+			}
+
+			if (prev == -1) {
+				prev = 4;
+			}
+
+			slides[current].classList.add("active");
+			slides[prev].classList.add("prev");
+			slides[next].classList.add("next");
+		}
+
+        const pressedButtonSelector = '[data-theme][aria-pressed="true"]';
+        const defaultTheme = 'eyes';
+        
+        const applyTheme = (theme) => {
+          const target = document.querySelector(`[data-theme="${theme}"]`);
+          document.documentElement.setAttribute("data-selected-theme", theme);
+          document.querySelector(pressedButtonSelector).setAttribute('aria-pressed', 'false');
+          target.setAttribute('aria-pressed', 'true');
+        };
+        
+        const handleThemeSelection = (event) => {
+          const target = event.target;
+          const isPressed = target.getAttribute('aria-pressed');
+          const theme = target.getAttribute('data-theme');        
+          
+          if(isPressed !== "true") {
+            applyTheme(theme);
+            localStorage.setItem('selected-theme', theme);
+          }
+        }
+        
+        const setInitialTheme = () => {
+          const savedTheme = localStorage.getItem('selected-theme');
+          if(savedTheme && savedTheme !== defaultTheme) {
+            applyTheme(savedTheme);
+          }
+        };
+        
+        setInitialTheme();
+        
+        const themeSwitcher = document.querySelector('.theme-switcher');
+        const buttons = themeSwitcher.querySelectorAll('button');
+        
+        buttons.forEach((button) => {
+           button.addEventListener('click', handleThemeSelection);
+        });
+        
+
 const degreeToRadian = (angle) => {
     return angle * (Math.PI / 180);
   };
@@ -54,3 +132,5 @@ const degreeToRadian = (angle) => {
     currentAngle += deltaAngle;
     circle.appendChild(span);
   });
+
+  
